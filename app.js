9b030d8484c1377b774a0c440e84079ffb3128f6
Fortunato <fortunato.maycotte@gmail.com>
2017-04-10 17:02:47 -0700

    creating different stores

--- app.js
+++ app.js
@@ -1,25 +1,65 @@
 'use strict';
 
-var custHour;
-var storeNames = ['una','dos'];
-
-var store = {
-  name: storeNames,
-  minHourly: 5,
-  maxHourly: 25,
+var storeHours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
+var firstAndPike = {
+  name: '1st and Pike',
+  minHourly: 23,
+  maxHourly: 65,
+  numCookiesPike: [],
+  avgSalesPerson: 6.3,
 };
 
-var minH = store.minHourly;
-var maxH = store.maxHourly;
+var minPike = firstAndPike.minHourly;
+var maxPike = firstAndPike.maxHourly;
 
 function custHour(min, max) {
-  min = parseInt(Math.ceil(min));
-  console.log(min);
-  max = parseInt(Math.floor(max));
-  console.log(max);
+  min = Math.ceil(min);
+  max = Math.floor(max);
   var total = Math.floor(Math.random() * (max - min)) + min;
-  console.log(total);
   return total;
 }
 
-custHour(minH, maxH);
+for (var hoursPike = 0;hoursPike < 14; hoursPike++) {
+  var cHour = custHour(minPike, maxPike);
+  firstAndPike.numCookiesPike.push(cHour);
+}
+console.log(firstAndPike.numCookiesPike);
+
+var storePikeUl = document.getElementById('store-list');
+console.log(storePikeUl);
+var storePikeLi;
+for (var i = 0; i < firstAndPike.numCookiesPike.length; i++ ){
+  storePikeLi = document.createElement('li');
+  storePikeLi.setAttribute('class', 'st-li');
+  storePikeLi.textContent = storeHours[i] + (firstAndPike.numCookiesPike[i] * firstAndPike.avgSalesPerson);
+  storePikeUl.appendChild(storePikeLi);
+}
+
+//Second store
+
+var firstAndPike = {
+  name: '1st and Pike',
+  minHourly: 23,
+  maxHourly: 65,
+  numCookiesPike: [],
+  avgSalesPerson: 6.3,
+};
+
+var minPike = firstAndPike.minHourly;
+var maxPike = firstAndPike.maxHourly;
+
+for (var hoursPike = 0;hoursPike < 14; hoursPike++) {
+  var cHour = custHour(minPike, maxPike);
+  firstAndPike.numCookiesPike.push(cHour);
+}
+console.log(firstAndPike.numCookiesPike);
+
+var storePikeUl = document.getElementById('store-list');
+console.log(storePikeUl);
+var storePikeLi;
+for (var i = 0; i < firstAndPike.numCookiesPike.length; i++ ){
+  storePikeLi = document.createElement('li');
+  storePikeLi.setAttribute('class', 'st-li');
+  storePikeLi.textContent = storeHours[i] + (firstAndPike.numCookiesPike[i] * firstAndPike.avgSalesPerson);
+  storePikeUl.appendChild(storePikeLi);
+}
